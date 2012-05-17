5e9e6dbca3503eee85bcc476bb92388ffac16774
Corey Oordt <coreyoordt@gmail.com>
2012-05-17 07:01:09 -0400

    Updated the Queued storage to parse container and properly transfer and delete local file.

--- apps/media_storage.py
+++ apps/media_storage.py
@@ -17,8 +17,23 @@ class MediaStorage(FileSystemStorage):
         super(MediaStorage, self).__init__(real_location, real_base_url, *args, **kwargs)
 
 class QueuedUserCloudFilesStorage(QueuedFileSystemStorage):
-    def __init__(self, remote='cumulus.storage.CloudFilesStorage', *args, **kwargs):
-        super(QueuedGridFSStorage, self).__init__(remote=remote, *args, **kwargs)
+    def __init__(self, local='media_storage.OverwriteStorage', 
+                       remote='media_storage.UserCloudFilesStorage', 
+                       task='queued_storage.tasks.TransferAndDelete',
+                       *args, **kwargs):
+        super(QueuedUserCloudFilesStorage, self).__init__(local=local, remote=remote, task=task, *args, **kwargs)
+
+class OverwriteStorage(FileSystemStorage):
+    """
+    Returns same name for existing file and deletes existing file on save.
+    """                                                              
+    def _save(self, name, content):
+        if self.exists(name):
+            self.delete(name)
+        return super(OverwriteStorage, self)._save(name, content)
+
+    def get_available_name(self, name):
+        return name
 
 class UserCloudFilesStorage(CloudFilesStorage):
     """
@@ -27,7 +42,17 @@ class UserCloudFilesStorage(CloudFilesStorage):
     
     In save, use that prefix to load a container instead of a directory.
     """
+    def get_available_name(self, name):
+        """
+        Return the name. If the file exists, we're going to overwrite it in _save
+        """
+        return name
+    
     def _save(self, name, content):
         (path, last) = os.path.split(name)
         self.container = self.connection.get_container(path)
-        super(UserCloudFilesStorage, self)._save(last, content)
+        if self.exists(name):
+            self.delete(name)
+        
+        output = super(UserCloudFilesStorage, self)._save(last, content)
+        return output
